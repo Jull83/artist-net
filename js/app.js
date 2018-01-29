89f67c2dad4ddd23db9aa590296b05e4220780d1
aidemar <aidemarmag@hotmail.com>
2018-01-29 13:41:43 -0600

    añadiendo funcionalidad

---
+++ js/app.js
@@ -0,0 +1,42 @@
+function archivo(evt) {
+      var files = evt.target.files; // FileList object
+       
+        //Obtenemos la imagen del campo "file".
+      for (var i = 0, f; f = files[i]; i++) {        
+           //Solo admitimos imágenes.
+           if (!f.type.match('image.*')) {
+                continue;
+           }
+       
+           var reader = new FileReader();
+           
+           reader.onload = (function(theFile) {
+               return function(e) {
+               // Creamos la imagen.
+                      document.getElementById("publish-image").innerHTML = ['<img class="thumb" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
+               };
+           })(f);
+ 
+           reader.readAsDataURL(f);
+       }
+}
+             
+document.getElementById('files').addEventListener('change', archivo, false);
+
+
+// los pasos para hacer tu función:
+// Crear evento
+// primero guarda el val() de tu input en una variable
+// luego creas todos los elementos que quieres que vayan en tu post
+// despues les pones atributos a esos elementos (en tu caso al <p> que creaste le pones un
+// text( y aqui dentro escribes el nombre de la variable del value del input)
+// despues colocas a cada hijo con su padre: <tu seccion de html>
+// <div de tu post>
+// <p>
+// con append
+
+var $text = $("#text-input");
+
+$("#add-text").click(paintText);
+
+function
